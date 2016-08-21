7471da112035a3240034527d7ad9fbfbc327805a
1 <1293431068@qq.com>
2016-08-21 16:27:59 +0800

    加阴影遮罩

--- app/scripts/drum.js
+++ app/scripts/drum.js
@@ -1,11 +1,17 @@
 $(document).ready(function(){
 	function getHeight(){
+		if($(window).width() <= 480){
+			$('.box.box-link').removeClass("box-sm");
+		}
+		else{
+			$('.box.box-link').addClass("box-sm");
+		}
+		
 		if($(window).width() <= 767){
 			var boxLgHeight = $(window).width();
 			var boxSmHeight = $(window).width() / 2;
 			$('.box.box-lg').css('height',boxLgHeight);
 			$('.box.box-sm').css('height',boxSmHeight);
-			
 		}
 		else{
 			$('.box.box-lg,.box.box-sm,.box-5 ,.box-6,.box-7,.box-8,.box-11,.box-13').removeAttr("style");
