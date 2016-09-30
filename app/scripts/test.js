5bd1a2340052a2f2228565577ee8ac5118e8d244
1 <1293431068@qq.com>
2016-09-30 16:27:17 +0800

    修改教师页面滚动

--- app/scripts/test.js
+++ app/scripts/test.js
@@ -31,32 +31,8 @@ $(document).ready(function(){
 
 	// }
 
-	//首屏向下滚动动效
-	 var presection_height = $('.vice-background').height();
-	 var boxes_height;
-	 var offset_top;
-	 function teacher_scroll(){
-	    $('.scroll-down').click(function() {
-	      	if($(window).width() >= 992){
-	        	boxes_height = $('.box-lg').height() + $('.box-sm').height();
-
-	      	}
-	      	else if(($(window).width() < 992) && ($(window).width() >= 480)){
-	        	boxes_height = 2*($('.box-lg').height() + $('.box-sm').height());
-	      	}
-	      	else if($(window).width() < 480){
-	        	boxes_height = $('.box-lg').height() + $('.box-2').height() + $('.box-3').height() + $('.box-4').height() + $('.box-4').height() + $('.box-6').height();
-	      	}
-	      	offset_top = presection_height + boxes_height;
-	      	$('body,html').animate({
-	        	scrollTop: offset_top
-	      	}, 600);
-	    });
-	}
-	teacher_scroll();
 	$(window).resize(function(){
 		getHeight();
-		teacher_scroll();
 		// if ($(window).width() < 480) {
 		// 	setTimeout(function(){
 		// 		$("#carousel2").carousel('pause');
@@ -69,5 +45,5 @@ $(document).ready(function(){
 		// 	$("#carousel2").carousel('cycle');
 
 		// }
-	});		
+	});	
 });
